6d17afb906579a91078c44b530fd36d8171bee72
ivansalinas <ivansalinasgarcia@gmail.com>
2024-03-02 20:21:22 +0100

    03/02/2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ module.exports = {
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
+    `./node_modules/tw-elements-react/dist/js/**/*.js`,
   ],
   theme: {
+    fontFamily: {
+      roboto: ["'Roboto', sans-serif"]
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements-react/dist/plugin")],
+  darkMode: "class",
 }
